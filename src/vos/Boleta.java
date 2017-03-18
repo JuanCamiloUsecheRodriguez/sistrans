b186c24205cb78c91ba5cefa51bf30709cd35154
Juan Camilo Usche <jc.useche10@uniandes.edu.co>
2017-03-18 18:00:03 -0500

    Alguas cosas

--- src/vos/Boleta.java
+++ src/vos/Boleta.java
@@ -1,5 +1,91 @@
 package vos;
 
+import org.codehaus.jackson.annotate.JsonProperty;
+
 public class Boleta {
 
+	/**
+	 * Id del video
+	 */
+	@JsonProperty(value="id")
+	private int id;
+	
+	/**
+	 * Numero de la silla
+	 */
+	@JsonProperty(value="silla")
+	private String silla;
+	
+	/**
+	 * Precio de la boleta
+	 */
+	@JsonProperty(value="precio")
+	private Integer precio;
+	
+	/**
+	 * Cupos
+	 */
+	@JsonProperty(value="cupos")
+	private Integer cupos;
+	
+	/**
+	 * Localidad
+	 */
+	@JsonProperty(value="localidad")
+	private String localidad;
+	
+	
+	
+	public Boleta(@JsonProperty(value="id")int id,	@JsonProperty(value="silla")String silla, @JsonProperty(value="precio")Integer precio,@JsonProperty(value="cupos") Integer cupos, @JsonProperty(value="localidad")String localidad) {
+		super();
+		this.id = id;
+		this.silla = silla;
+		this.precio = precio;
+		this.cupos = cupos;
+		this.localidad = localidad;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSilla() {
+		return silla;
+	}
+
+	public void setSilla(String silla) {
+		this.silla = silla;
+	}
+
+	public Integer getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Integer precio) {
+		this.precio = precio;
+	}
+
+	public Integer getCupos() {
+		return cupos;
+	}
+
+	public void setCupos(Integer cupos) {
+		this.cupos = cupos;
+	}
+
+	public String getLocalidad() {
+		return localidad;
+	}
+
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+
+	
+	
+	
 }

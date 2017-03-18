9b7b8c7fd884ec03ca5e5905eb55cf26bff4c7c0
osabogal10 <o.sabogal10@uniandes.edu.co>
2017-03-18 18:03:01 -0500

    vos

--- src/vos/Funcion.java
+++ src/vos/Funcion.java
@@ -1,5 +1,81 @@
 package vos;
 
+import java.sql.Date;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
 public class Funcion {
+	
+	@JsonProperty(value="id")
+	private int id;
+	
+	@JsonProperty(value="fecha")
+	private Date fecha;
+	
+	@JsonProperty(value="sitio")
+	private Sitio sitio;
+	
+	@JsonProperty(value="espectaculo")
+	private Espectaculo espectaculo;
+	
+	@JsonProperty(value="realizada")
+	private boolean realizada;
+
+	public Funcion(
+			@JsonProperty(value="id")int id,
+			@JsonProperty(value="fecha")Date fecha, 
+			@JsonProperty(value="sitio")Sitio sitio, 
+			@JsonProperty(value="espectaculo")Espectaculo espectaculo, 
+			@JsonProperty(value="realizada")boolean realizada) {
+		super();
+		this.id = id;
+		this.fecha = fecha;
+		this.sitio = sitio;
+		this.espectaculo = espectaculo;
+		this.realizada = realizada;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public Sitio getSitio() {
+		return sitio;
+	}
+
+	public void setSitio(Sitio sitio) {
+		this.sitio = sitio;
+	}
+
+	public Espectaculo getEspectaculo() {
+		return espectaculo;
+	}
+
+	public void setEspectaculo(Espectaculo espectaculo) {
+		this.espectaculo = espectaculo;
+	}
+
+	public boolean isRealizada() {
+		return realizada;
+	}
+
+	public void setRealizada(boolean realizada) {
+		this.realizada = realizada;
+	}
+	
+	
+	
 
 }
--- src/vos/Sitio.java
+++ src/vos/Sitio.java
@@ -1,5 +1,118 @@
 package vos;
 
+import java.sql.Date;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
 public class Sitio {
+	
+	@JsonProperty(value="id")
+	private int id;
+	
+	@JsonProperty(value="nombre")
+	private String nombre;
+	
+	@JsonProperty(value="tipo")
+	private String tipo;
+	
+	@JsonProperty(value="cupos")
+	private int cupos;
+	
+	@JsonProperty(value="accesibilidad")
+	private boolean accesibilidad;
+	
+	@JsonProperty(value="horaInicio")
+	private Date horaInicio;
+	
+	@JsonProperty(value="horaFin")
+	private Date horaFin;
+	
+	@JsonProperty(value="tipoSilleteria")
+	private String tipoSilleteria;
+
+	public Sitio(
+			@JsonProperty(value="id")int id,
+			@JsonProperty(value="nombre") String nombre,
+			@JsonProperty(value="tipo") String tipo,
+			@JsonProperty(value="cupos") int cupos,
+			@JsonProperty(value="accesibilidad") boolean accesibilidad,
+			@JsonProperty(value="horaInicio")Date horaInicio,
+			@JsonProperty(value="horaFin")Date horaFin,
+			@JsonProperty(value="tipoSilleteria")String tipoSilleteria) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.tipo = tipo;
+		this.cupos = cupos;
+		this.accesibilidad = accesibilidad;
+		this.horaInicio = horaInicio;
+		this.horaFin = horaFin;
+		this.tipoSilleteria = tipoSilleteria;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public int getCupos() {
+		return cupos;
+	}
+
+	public void setCupos(int cupos) {
+		this.cupos = cupos;
+	}
+
+	public boolean isAccesibilidad() {
+		return accesibilidad;
+	}
+
+	public void setAccesibilidad(boolean accesibilidad) {
+		this.accesibilidad = accesibilidad;
+	}
+
+	public Date getHoraInicio() {
+		return horaInicio;
+	}
+
+	public void setHoraInicio(Date horaInicio) {
+		this.horaInicio = horaInicio;
+	}
+
+	public Date getHoraFin() {
+		return horaFin;
+	}
+
+	public void setHoraFin(Date horaFin) {
+		this.horaFin = horaFin;
+	}
+
+	public String getTipoSilleteria() {
+		return tipoSilleteria;
+	}
+
+	public void setTipoSilleteria(String tipoSilleteria) {
+		this.tipoSilleteria = tipoSilleteria;
+	}
+	
 
 }

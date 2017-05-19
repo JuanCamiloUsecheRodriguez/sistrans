8c1139699ec04f3f71efb7c3a126ca4e5e922c54
osabogal10 <o.sabogal10@uniandes.edu.co>
2017-05-19 11:44:20 -0500

    it5

---
+++ src/dtm/FestivAndesDistributed.java
@@ -0,0 +1,112 @@
+package dtm;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.jms.JMSException;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.TopicConnectionFactory;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+
+import com.rabbitmq.jms.admin.RMQConnectionFactory;
+import com.rabbitmq.jms.admin.RMQDestination;
+
+import tm.FestivAndesMaster;
+
+public class FestivAndesDistributed {
+
+	private final static String QUEUE_NAME = "java:global/RMQAppQueue";
+	private final static String MQ_CONNECTION_NAME = "java:global/RMQClient";
+	
+	private static FestivAndesDistributed instance;
+	
+	private FestivAndesMaster tm;
+	
+	private QueueConnectionFactory queueFactory;
+	
+	private TopicConnectionFactory factory;
+	
+	//private AllVideosMDB allVideosMQ;
+	
+	private static String path;
+
+
+	private FestivAndesDistributed() throws NamingException, JMSException
+	{
+		InitialContext ctx = new InitialContext();
+		factory = (RMQConnectionFactory) ctx.lookup(MQ_CONNECTION_NAME);
+		allVideosMQ = new AllVideosMDB(factory, ctx);
+		
+		allVideosMQ.start();
+		
+	}
+	
+	public void stop() throws JMSException
+	{
+		allVideosMQ.close();
+	}
+	
+	/**
+	 * Método que retorna el path de la carpeta WEB-INF/ConnectionData en el deploy actual dentro del servidor.
+	 * @return path de la carpeta WEB-INF/ConnectionData en el deploy actual.
+	 */
+	public static void setPath(String p) {
+		path = p;
+	}
+	
+	public void setUpTransactionManager(FestivAndesMaster tm)
+	{
+	   this.tm = tm;
+	}
+	
+	private static FestivAndesDistributed getInst()
+	{
+		return instance;
+	}
+	
+	public static FestivAndesDistributed getInstance(FestivAndesMaster tm)
+	{
+		if(instance == null)
+		{
+			try {
+				instance = new FestivAndesDistributed();
+			} catch (NamingException e) {
+				e.printStackTrace();
+			} catch (JMSException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		instance.setUpTransactionManager(tm);
+		return instance;
+	}
+	
+	public static FestivAndesDistributed getInstance()
+	{
+		if(instance == null)
+		{
+			FestivAndesMaster tm = new FestivAndesMaster(path);
+			return getInstance(tm);
+		}
+		if(instance.tm != null)
+		{
+			return instance;
+		}
+		FestivAndesMaster tm = new FestivAndesMaster(path);
+		return getInstance(tm);
+	}
+	
+	public ListaVideos getLocalVideos() throws Exception
+	{
+		return tm.darVideosLocal();
+	}
+	
+	public ListaVideos getRemoteVideos() throws JsonGenerationException, JsonMappingException, JMSException, IOException, NonReplyException, InterruptedException, NoSuchAlgorithmException
+	{
+		return allVideosMQ.getRemoteVideos();
+	}
+}
